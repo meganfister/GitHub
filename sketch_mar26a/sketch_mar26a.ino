73c84f4230cac1e4fd1ad93e2a26568ddf47d560
meganfister <mfi256@uky.edu>
2025-03-26 21:59:37 -0400

    Delete control pin 1 and 2

--- sketch_mar26a/sketch_mar26a.ino
+++ sketch_mar26a/sketch_mar26a.ino
@@ -6,6 +6,4 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   const int PWMB = 10; //speed control pin on the motor driver for the left motor
-  const int BIN2 = 9; //control pin 2 on the motor driver for the left motor
-  const int BIN1 = 8; //control pin 1 on the motor driver for the left motor
 }
